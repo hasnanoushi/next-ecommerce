909ac0b0aa74360d922240cbb9946e66f845914c
HASNA KKP <hasnanoushi66@gmail.com>
2024-06-03 19:33:21 +0530

    key updated

--- src/app/lib/commerce.js
+++ src/app/lib/commerce.js
@@ -1,5 +1,5 @@
 import CommerceSDK from "@chec/commerce.js";
 
-const client = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY);
+const client = new CommerceSDK("pk_5706029c2e2bfc0eedb4b7868770e08a6ac575a64e2fc");
 
 export default client;
